c818f10f4e5a9543541c1483c24a14df6f83a9e2
Marcelo Biotto <lee.am@live.com>
2024-03-03 14:30:45 -0300

    criando acessarConta e menuAcessarConta

--- src/script.js
+++ src/script.js
@@ -1,23 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     let menuPrincipal = document.querySelector('.menuPrincipal');
-    let sairBtn = document.querySelector('#sair');
-    let acessarContaBtn = document.querySelector('#acessarConta');
+  
 
     menuPrincipal.addEventListener('click', (e)=>{
         e.preventDefault();
+        
         window.location.href = "./menuPrincipal.html";
     })
 
-    sairBtn.addEventListener('click', (e)=> {
-        e.preventDefault();
-        window.location.href = "./index.html";
-        
-    });
 
-    acessarContaBtn.addEventListener('click', (e)=>{
-        e.preventDefault();
-        console.log('tentaram me acessar!')
-    })
+
 
 });
