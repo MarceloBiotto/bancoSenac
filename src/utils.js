788889552afa92173249e347fbbd2bdd3d290e4c
Marcelo Biotto <lee.am@live.com>
2024-03-06 05:56:12 -0300

    alterando html

--- src/utils.js
+++ src/utils.js
@@ -2,11 +2,15 @@ import { arrayTodasContas } from './acessarConta.js';
 
 let verSaldo = document.querySelector('#btn-verSaldo');
 verSaldo.addEventListener('click', () => {
+
     let saldoTotal = "";
     
+
     arrayTodasContas.forEach(conta => {
+   
         saldoTotal += `R$ ${conta.saldo} <br>`;
     });
+
     verSaldo.innerHTML = saldoTotal;
 });
 
